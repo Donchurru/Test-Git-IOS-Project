dabc345e880824db32092a71a398efce4a542258
Ruben Romero <mac@MacBook-Pro-de-Ruben.local>
2014-09-18 01:05:25 +0200

    added a test class

---
+++ Git Test/DCTest.m
@@ -0,0 +1,13 @@
+//
+//  DCTest.m
+//  Git Test
+//
+//  Created by Ruben Romero on 18/09/14.
+//  Copyright (c) 2014 Donchurru. All rights reserved.
+//
+
+#import "DCTest.h"
+
+@implementation DCTest
+
+@end
